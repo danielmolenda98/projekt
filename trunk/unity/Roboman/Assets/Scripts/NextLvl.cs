746479ce21b96a6e18f8f30d71ccebdd9ed11b66
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-07 08:55:01 +0000

    Project setup

---
+++ trunk/unity/Roboman/Assets/Scripts/NextLvl.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class NextLvl : MonoBehaviour {
+
+	public int nextlvl;
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.transform.tag == "Gracz") {
+			Application.LoadLevel (nextlvl);
+		}
+	
+	}
+}
---
+++ trunk/unity/Roboman/Assets/Scripts/Patrol.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+
+	public Transform[] patrolPoints;
+	public float moveSpeed;
+	private int currentPoint;
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector2(transform.localScale.x,transform.localScale.y);
+		transform.position=patrolPoints[0].position;
+		currentPoint=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position == patrolPoints[currentPoint].position)
+		{
+			transform.localScale = new Vector2(-transform.localScale.x,transform.localScale.y);
+			currentPoint++;
+		}
+		if(currentPoint >= patrolPoints.Length)
+		{
+			currentPoint=0;
+		}
+		transform.position = Vector2.MoveTowards(transform.position,patrolPoints[currentPoint].position,moveSpeed=Time.deltaTime);
+	}
+}
