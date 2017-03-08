eae055a521de754fb8f5496feb42c347bd136a91
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-08 13:07:09 +0000

    death

--- unity/Roboman/Assets/Scripts/Death.cs
+++ unity/Roboman/Assets/Scripts/Death.cs
@@ -5,14 +5,18 @@ using System;
 public class Death : MonoBehaviour
 {
     public Vector3 spawnPoint;
+    public float respawnTime = 3.0f;
 
-    void OnCollisionEnter2D(Collision2D other)
+    void Start()
     {
-        if (other.gameObject.name == "Player")
-        {
-            new WaitForSeconds(2.0f);
-            other.transform.position = spawnPoint;
-            transform.position = new Vector3(0.0f, 0.0f, -5.0f);
-        }
+        spawnPoint = gameObject.transform.position;
+        StartCoroutine(Respawn());
+    }
+
+    IEnumerator Respawn()
+    {
+        // Te dwie linijki potrzebne tylko do pokazania efektu
+        yield return new WaitForSeconds(respawnTime);
+        gameObject.transform.position = spawnPoint;
     }
 }
