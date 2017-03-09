4447009c1ee83b90684bd3d0bb07720f293e9fd5
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-09 21:44:01 +0000

    life work

---
+++ trunk/unity/Roboman/Assets/Scripts/LifeLoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeLoss : MonoBehaviour
+{
+    public uiManager uiObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        uiObj = GameObject.FindObjectOfType(typeof(uiManager)) as uiManager;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            uiObj.lifesUpdate();
+        }
+    }
+}
--- trunk/unity/Roboman/Assets/Scripts/uiManager.cs
+++ trunk/unity/Roboman/Assets/Scripts/uiManager.cs
@@ -5,23 +5,64 @@ using UnityEngine.UI;
 
 public class uiManager : MonoBehaviour
 {
-    public Text pointText;
-    int point;
+    public Text pointsText;
+
+    public RawImage lifeCPU;
+    public RawImage lifeCPU1;
+    public RawImage lifeCPU2;
+
+    int cur_points;
+    int cur_lifes;
 
     // Use this for initialization
     void Start()
     {
-        point = 0;
+        cur_points = 0;
+        cur_lifes = 3;
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        pointText.text = "Points: " + point;
+        pointsText.text = "Points: " + cur_points;
+
+        if (cur_lifes == 3)
+        {
+            lifeCPU.enabled = true;
+            lifeCPU1.enabled = true;
+            lifeCPU2.enabled = true;
+        }
+
+        if (cur_lifes == 2)
+        {
+            lifeCPU.enabled = true;
+            lifeCPU1.enabled = true;
+            lifeCPU2.enabled = false;
+        }
+
+        if (cur_lifes == 1)
+        {
+            lifeCPU.enabled = true;
+            lifeCPU1.enabled = false;
+            lifeCPU2.enabled = false;
+        }
+
+        if (cur_lifes == 0)
+        {
+            lifeCPU.enabled = false;
+            lifeCPU1.enabled = false;
+            lifeCPU2.enabled = false;
+        }
     }
 
-    public void pointUpdate()
+    public void pointsUpdate()
     {
-        point++;
+        cur_points++;
+    }
+
+    public void lifesUpdate()
+    {
+        if(cur_lifes > 0)
+            cur_lifes--;
     }
 }
