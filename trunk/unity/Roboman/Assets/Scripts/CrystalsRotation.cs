1f238d8f907db2af0035ed18385bfe049262f7ca
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-14 11:26:26 +0000

    CrystalsRotation.cs

---
+++ trunk/unity/Roboman/Assets/Scripts/CrystalsRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalsRotation : MonoBehaviour
+{
+    public float speed = 2f; 
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0,speed, 0);	
+	}
+}
