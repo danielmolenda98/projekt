c6a533cd7d4c5c350c53b71418e2668ec65eed36
dawid.duda <dawid.duda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-14 13:17:28 +0000

    upddate bad

---
+++ trunk/unity/Roboman/Assets/Scripts/spring.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spring : MonoBehaviour
+{
+    public RigidbodyConstraints constraints;
+    public bool freezePosition;
+    private Rigidbody2D rigidbody;
+    public float jumpHeight;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    public LayerMask mask;
+
+    private Rigidbody rb;
+
+    void Update()
+    {
+       
+            if (collision.GameObjectTag == "Player")
+            {
+                
+                rigidbody.AddForce(Vector2.up * 1200);
+
+                if (GameObjectTag.JumpHeight = 1200)
+                {
+                    rigidbody.velocity = Vector3.zero;
+
+
+                }
+            }
+
+    }
+
+}
