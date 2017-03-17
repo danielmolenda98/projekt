76d5b899652d4e560c4b7581e57229a6ed4c2262
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-17 14:45:42 +0000

    Menu pause

---
+++ trunk/unity/Roboman/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+    private bool paused = false;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }		
+
+        if(paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
+        if (!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+	}
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
