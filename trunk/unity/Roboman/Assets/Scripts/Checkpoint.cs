7791e9a6a86aaf77fff7dfbd465f56d43e6e2f8b
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-20 10:34:52 +0000

    jeeej działa pumpkin (oby)

---
+++ trunk/unity/Roboman/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        levelManager = FindObjectOfType<LevelManager>();	
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            levelManager.RespawnPlayer();
+        }
+    }
+}
---
+++ trunk/unity/Roboman/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Vector2 currentCheckpoint;
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = FindObjectOfType <PlayerController> ();
+	}
+	
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player Respawn");
+        player.transform.position = currentCheckpoint;
+    }
+}
