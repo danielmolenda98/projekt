856a8b400b1c1dab2ff2a1ee6f501959c4ffdf14
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-22 09:45:12 +0000

    spring

---
+++ trunk/unity/Roboman/Assets/Scripts/Spring1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring1 : MonoBehaviour
+{
+    public float jumpHeight;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Rigidbody2D rgdBody = collision.gameObject.GetComponent<Rigidbody2D>();
+            rgdBody.AddForce(Vector2.up * 1240);
+            if (jumpHeight == 1240) ;
+            rgdBody.velocity = Vector3.zero;
+        }
+    }
+}
