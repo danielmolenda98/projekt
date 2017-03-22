528000d620eee82532cf7f58926a772debb6b76b
daniel.molenda <daniel.molenda@fcf92098-4ae7-4b32-819d-efc4ea43536c>
2017-03-22 13:08:47 +0000

    patrol

---
+++ unity/Roboman/Assets/Scripts/MovingEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEnemy : Enemy
+{
+    public Vector2 destinationPoint;
+    private Vector2[] destinationsPoints;
+    public float moveSpeed;
+    private int currentPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        destinationsPoints = new Vector2[2];
+        destinationsPoints[0] = transform.position;
+        destinationsPoints[1] = new Vector2(transform.position.x + destinationPoint.x, transform.position.y + destinationPoint.y);
+        currentPoint = 0;
+ 
+        if (destinationPoint.x > 0)
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+    }
+
+    void Update()
+    {
+        if (new Vector2(transform.position.x, transform.position.y) == destinationsPoints[currentPoint])
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            currentPoint++;
+        }
+        if (currentPoint >= destinationsPoints.Length)
+        {
+            currentPoint = 0;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, destinationsPoints[currentPoint], moveSpeed = Time.deltaTime);
+    }
+}
